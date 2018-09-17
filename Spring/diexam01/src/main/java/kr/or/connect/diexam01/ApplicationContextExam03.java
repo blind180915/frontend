3e6787914659c10d9d7cafc324c0877ce24006d1
heesu <sw2725@naver.com>
2018-09-17 23:14:16 +0900

    Java Config를 이용한 설정.

---
+++ Spring/diexam01/src/main/java/kr/or/connect/diexam01/ApplicationContextExam03.java
@@ -0,0 +1,17 @@
+package kr.or.connect.diexam01;
+ 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class ApplicationContextExam03 {
+ 
+    public static void main(String[] args) {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        
+//      Car car = (Car)ac.getBean("car");
+        // 클래스 타입을 넣어주기 때문에 메소드명과 관계없이 연결되어 실수를 줄일 수 있다.
+        Car car = ac.getBean(Car.class);
+        car.run();
+    }
+}
