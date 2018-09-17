42e3ade6d2710ce931f82bcbc8827f92b4a28488
heesu <sw2725@naver.com>
2018-09-17 22:28:47 +0900

    Dependency Injection

---
+++ Spring/diexam01/src/main/java/kr/or/connect/diexam01/Car.java
@@ -0,0 +1,26 @@
+package kr.or.connect.diexam01;
+ 
+public class Car {
+    public Engine v8;
+    
+    public Car() {
+        System.out.println("Car 생성자");
+    }
+    
+    public void setEngine(Engine e) {
+        this.v8 = e;
+    }
+    
+    public void run() {
+        System.out.println("엔진을 이용하여 달립니다.");
+        v8.exec();
+    }
+    
+    // DI를 하지 않고 실행할 경
+    public static void main(String[] args) {
+        Engine e = new Engine();
+        Car c = new Car();
+        c.setEngine(e);
+        c.run();
+    }
+}
